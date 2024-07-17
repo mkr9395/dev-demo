4e7e37e3deb915f755762596625176f002acaab6
Mohit Kumar <mkr9395@gmail.com>
2024-07-17 18:55:58 +0530

    add first experiment

---
+++ src/data_ingestion.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+import numpy as np 
+import pathlib
+import os
+
+from pathlib import Path
+
+
+df = pd.read_csv('https://raw.githubusercontent.com/araj2/customer-database/master/Ecommerce%20Customers.csv')
+
+df = df.iloc[:,3:]
+
+df = df[df['Length of Membership'] > 3]
+
+joined_path = Path.joinpath(Path.cwd() / 'data')
+
+
+
+df.to_csv(joined_path/'customer.csv',index=False)
